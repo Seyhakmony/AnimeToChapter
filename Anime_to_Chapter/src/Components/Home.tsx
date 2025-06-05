fd31ad8aa584d17ce5bf9206157699d362fde080
Seyhakmony <158637575+Seyhakmony@users.noreply.github.com>
2025-06-05 04:23:57 -0400

    Update Home.tsx

--- Anime_to_Chapter/src/Components/Home.tsx
+++ Anime_to_Chapter/src/Components/Home.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import '../Css/HomeSearch.css';
-import '../Css/homeExtra.css';
+import '../Css/HomeExtra.css';
 
 interface AnimeResult {
   mal_id: number;
